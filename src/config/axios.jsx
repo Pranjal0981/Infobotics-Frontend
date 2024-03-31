ed6b0c2db80c2342876b1ae4b752cf4289024bf6
Pranjal Shukla <pranjalshukla245@gmail.com>
2024-04-01 00:19:05 +0530

    update

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL:
-         "https://info-back.vercel.app/",
+         "https://info-back.onrender.com/",
     withCredentials: true,
 });
 
