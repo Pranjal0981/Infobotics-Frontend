1c3c8258c0ef5c741b9d29418b7dbc139ae32174
Pranjal0981 <pkshukla988@gmail.com>
2024-04-05 10:37:44 +0530

    update

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -3,7 +3,7 @@ const token = window.localStorage.getItem("token")
 const instance = axios.create(
 
     {
-        baseURL: "http://localhost:3000",
+        baseURL: "https://info-back.onrender.com/",
         withCredentials: true,
         headers: {
             Authorization: token
