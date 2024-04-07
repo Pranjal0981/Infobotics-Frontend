689500d2c7e8cbb61eb1e305a9fe992ac9fcce6a
Pranjal0981 <pkshukla988@gmail.com>
2024-04-07 13:18:25 +0530

    ss

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL:
-        "https://info-back.vercel.app/",
+        "https://infobotics.vercel.app/",
     withCredentials: true,
 });
 
